543dd62391d5b3dc466ca856593cf095a939d72a
Vinícius Nunes Menti <virotti@gmail.com>
2023-09-19 17:02:59 -0300

    Content created

---
+++ src/app/data/datafake.ts
@@ -0,0 +1,27 @@
+export  const dataFake = [
+    {
+        "id": "1",
+        "title": "Do flying horses exist?",
+        "description": "In a remarkable event, a flying white horse was spotted in the clouds by a bystander in the middle of the night. The ethereal image, which appeared to be a majestic white horse galloping across the night sky, left the onlooker in awe.",
+        "photoCover": "https://images.nightcafe.studio/jobs/33Q6ojVmPjWqZm2EgpqY/33Q6ojVmPjWqZm2EgpqY--4--rqx49_4x.jpg?tr=w-1080,c-at_max",
+    },
+    {
+        "id": "2",
+        "title": "Man sees the Universe with eyes closed",
+        "description": "In this thought-provoking article, we delve into the extraordinary journey of a man who claims to see the universe with his eyes closed. Through meditation and sensory deprivation, he explores the vast expanses of his mind, which he believes mirrors the cosmos. His experiences challenge our understanding of perception and reality, inviting readers to question what it truly means to ‘see’.",
+        "photoCover": "https://images.nightcafe.studio/jobs/pNL1AhFFmDFFnwi9xpZx/pNL1AhFFmDFFnwi9xpZx--1--wibdi_13.8889x.jpg?tr=w-1080,c-at_max",
+    },
+    {
+        "id": "3",
+        "title": "Woman from Jupiter came to Earth",
+        "description": "In this intriguing article, we introduce a woman who claims to have journeyed from Jupiter to Earth. Despite the impossibility of life as we know it on the gas giant, she shares her unique experiences and insights, challenging our preconceptions about life in our solar system. This tale of interplanetary travel invites readers to expand their horizons and imagine the unexplored possibilities of existence.",
+        "photoCover": "https://images.nightcafe.studio/jobs/fs3bGfo1yrkljJlKXtVE/fs3bGfo1yrkljJlKXtVE--1--r5xd8_15.625x.jpg?tr=w-1080,c-at_max",
+    },
+    {
+        "id": "4",
+        "title": "Woman from Venus talks about love",
+        "description": "In this captivating article, we meet a woman who identifies herself as being from Venus, the planet synonymous with love. She shares her unique perspective on love and relationships, drawing parallels between her ‘home’ planet’s qualities and her own philosophies. This tale of cosmic wisdom invites readers to reconsider their understanding of love and to embrace a more universal, Venusian kind of love.",
+        "photoCover": "https://images.nightcafe.studio/jobs/GNEoi8cquGIPoKzIAO30/GNEoi8cquGIPoKzIAO30--1--lr6tp_7.8125x.jpg?tr=w-1080,c-at_max",
+    }
+]
+
